0535fe40fb3a35e9fee5bb073ec88ed1fc86e88f
Justin Goats <justingoats@yahoo.com>
2021-09-13 12:24:48 -0700

    ability to switch between viewing a single article or multiple article cards

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -2,14 +2,28 @@ import React from "react";
 import "./landingpage.scss";
 import Nav from "../nav/nav";
 import axios from "axios";
+import SingleArticle from "../singlearticle/singlearticle.js";
 
 export default class LandingPage extends React.Component {
     constructor() {
         super();
         this.state = {
             blogs: [],
-            content: []
+            singlePage: null,
+            images: []
         }
+        this.handleSinglePage = this.handleSinglePage.bind(this);
+        this.clearBlog = this.clearBlog.bind(this);
+    }
+    handleSinglePage(content) {
+        this.setState({
+            singlePage: content
+        })
+    }
+    clearBlog() {
+        this.setState({
+            singlePage: null
+        })
     }
     componentDidMount() {
         axios({
@@ -24,29 +38,40 @@ export default class LandingPage extends React.Component {
             this.setState({
                 blogs: concated
             })
-            for (let i = 0; i < result.data.blogs.length; i++) {
-                document.getElementsByClassName("screenshot")[i].innerHTML += result.data.blogs[i].screenshot;
-            }
-
         }).catch((err) => {
             console.log(err);
         })
     }
     render() {
-        return (
-            <div className="landingpage-background">
-                <Nav />
-                <div className="articles">
-                    {this.state.blogs.map((item) =>
-                        <div className="blog-card" key={item._id}>
-                            <div className="screenshot"></div>
-                            <div className="blog-title">{item.title}</div>
-                            <div className="blog-description">{item.description}</div>
-                            <div className="blog-timestamp">{new Date(item.time).toLocaleDateString()}</div>
-                        </div>
-                    )}
+        if (!this.state.singlePage) {
+            return (
+                <div className="landingpage-background">
+                    <Nav />
+                    <div className="articles">
+                        {this.state.blogs.map((item) =>
+                            <div onClick={() => this.handleSinglePage({
+                                title: item.title,
+                                description: item.description,
+                                content: item.content,
+                                time: item.time,
+                                id: item._id,
+                                screenshot: item.screenshot
+                            })} className="blog-card" key={item._id}>
+                                <div className="screenshot">
+                                    <img className="blog-image" src={item.screenshot} /></div>
+                                <div className="blog-title">{item.title}</div>
+                                <div className="blog-description">{item.description}</div>
+                                <div className="blog-timestamp">{new Date(item.time).toLocaleDateString()}</div>
+                            </div>
+                        )}
+                    </div>
                 </div>
-            </div>
-        );
-    }
-}
+            );
+        }
+        else {
+            return (
+                <SingleArticle clearBlog={this.clearBlog} article={this.state.singlePage} />
+            )
+        }
+    } // render
+} // conponent
