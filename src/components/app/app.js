b200268257d65228cb087513f9e896436de81531
Justin Goats <justingoats@yahoo.com>
2021-09-20 21:45:46 -0700

    added script tag for index.bundle.js on index.html page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
-import { useState, useEffect } from "react";
 import AdminLogin from "../adminlogin/adminlogin";
 import Dashboard from "../dashboard/dashboard";
 import LandingPage from "../landingpage/landingpage";
