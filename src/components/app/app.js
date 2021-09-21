e823b122267aed56fe6c2ca8a1513b5fc1ec2dfb
Justin Goats <justingoats@yahoo.com>
2021-09-20 23:33:56 -0700

    commit to master

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,17 +5,18 @@ import Dashboard from "../dashboard/dashboard";
 import LandingPage from "../landingpage/landingpage";
 import Videos from "../videos/videos.js";
 import Articles from "../articles/articles.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App(props) {
 
     return (
-        <>
+        <Router>
             <Route path="/videos" render={props => <Videos />} />
             <Route path="/articles" render={props => <Articles />} />
             <Route path="/admin" render={props => <AdminLogin />} />
             <Route path="/dashboard" render={props => <Dashboard />} />
             <Route path="/" exact render={props => <LandingPage />} />
-        </>
+        </Router>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/app/app.js";
-import { BrowserRouter as Router } from "react-router-dom";
 
 
-ReactDom.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDom.render(React.createElement(<App />), document.getElementById("root"));
